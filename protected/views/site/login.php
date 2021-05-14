2b0813ca532c64eeddd16b8f30326ea2bdaf6a4d
Lionnel Leunkeu <lleunkeu@gmail.com>
2021-05-14 21:46:19 +0100

    Updated login form structure

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -3,51 +3,67 @@
 /* @var $model LoginForm */
 /* @var $form CActiveForm  */
 
-$this->pageTitle=Yii::app()->name . ' - Login';
-$this->breadcrumbs=array(
-	'Login',
+$this->pageTitle = Yii::app()->name . ' - Login';
+$this->breadcrumbs = array(
+    'Login',
 );
 ?>
 
-<h1>Login</h1>
+<div class="container h-75" id="page">
+    <section class="my-5">
+        <div class="form w-50 p-3 m-auto">
+        <h1>Login</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
+        <p>Please fill out the following form with your login credentials:</p>
 
-<div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
+            <?php $form = $this->beginWidget('CActiveForm', array(
+                'id' => 'login-form',
+                'enableClientValidation' => true,
+                'clientOptions' => array(
+                    'validateOnSubmit' => true,
+                ),
+                'htmlOptions' => array(
+                    'class' => 'row gy-2 gx-3 align-items-center',
+                ),
+            )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+            <p class="note mb-5">Fields with <span class="required">*</span> are required.</p>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+            <div class="row mb-3">
+                <?php echo $form->labelEx($model, 'username', ['class' => 'col-sm-3 col-form-label']); ?>
+                <div class="col-sm-9">
+                    <?php echo $form->textField($model, 'username', ['class' => 'form-control']); ?>
+                    <?php echo $form->error($model, 'username'); ?>
+                </div>
+            </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-	</div>
+            <div class="row mb-3">
+                <?php echo $form->labelEx($model, 'password', ['class' => 'col-sm-3 col-form-label']); ?>
+                <div class="col-sm-9">
+                    <?php echo $form->passwordField($model, 'password', ['class' => 'form-control']); ?>
+                    <?php echo $form->error($model, 'password'); ?>
+                    <p class="hint d-none">
+                        Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
+                    </p>
+                </div>
+            </div>
 
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
+            <div class="row mb-3 rememberMe">
+                <label for="colFormLabelLg" class="col-sm-3 col-form-label col-form-label-lg">&nbsp;</label>
+                <div class="col-sm-9 form-check">
+                    <?php echo $form->checkBox($model, 'rememberMe', ['class' => ' pl-2 form-check-input']); ?>
+                    <?php echo $form->label($model, 'rememberMe', ['class' => 'form-check-label'] ); ?>
+                    <?php echo $form->error($model, 'rememberMe'); ?>
+                </div>
+            </div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
+            <div class="row buttons">
+                <div class="col-12 text-center">
+                    <?php echo CHtml::submitButton('Login', ['class' => 'btn btn-primary ']); ?>
+                </div>
+            </div>
 
-<?php $this->endWidget(); ?>
-</div><!-- form -->
+            <?php $this->endWidget(); ?>
+        </div><!-- form -->
+    </section><!-- form -->
+</div><!-- container -->
