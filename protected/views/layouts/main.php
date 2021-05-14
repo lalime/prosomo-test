64dd7439ba437967a7378d53d295bded10e98ff0
Lionnel Leunkeu <lleunkeu@gmail.com>
2021-05-14 21:20:15 +0100

    Added bootstrap framework and home page structure

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,59 +1,78 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="language" content="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
-	<![endif]-->
+    <!-- blueprint CSS framework -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print"> -->
+    <!--[if lt IE 8]>
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
+        <![endif]-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css"> -->
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
 
-<div class="container" id="page">
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+    <div class="_container" id="page">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container -fluid">
+                <a class="navbar-brand" href="#" title="<?php echo CHtml::encode(Yii::app()->name); ?>">PHPP</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <?php $this->widget('zii.widgets.CMenu', array(
+                        'htmlOptions' => array('class' => 'navbar-nav'),
+                        'itemCssClass' => 'nav-item',
+                        'linkLabelWrapperHtmlOptions' => 'nav-link',
+                        'activeCssClass' => 'active',
+                        'items' => array(
+                            array('label' => 'Home', 'linkOptions' => array('class' => 'nav-link', 'activeCssClass' => 'active'), 'url' => array('/site/index')),
+                            array('label' => 'About', 'linkOptions' => array('class' => 'nav-link'), 'url' => array('/site/page', 'view' => 'about')),
+                            array('label' => 'Contact', 'linkOptions' => array('class' => 'nav-link'), 'url' => array('/site/contact')),
+                            array('label' => 'Login', 'linkOptions' => array('class' => 'nav-link'), 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                            array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                        ),
+                    )); ?>
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+                </div>
+            </div>
+        </nav>
 
-	<?php echo $content; ?>
 
-	<div class="clear"></div>
+        <?php if (isset($this->breadcrumbs)) : ?>
+            <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+                'links' => $this->breadcrumbs,
+            )); ?>
+            <!-- breadcrumbs -->
+        <?php endif ?>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
+        <?php echo $content; ?>
 
-</div><!-- page -->
+        <div class="clear"></div>
+
+        <div class="footer-copyright bg-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <p class="text-white my-3">Copyright <?php echo CHtml::encode(Yii::app()->name); ?> © <?php echo Yii::powered(); ?>. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div><!-- footer -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    </div><!-- page -->
 
 </body>
+
 </html>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,25 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
+
+// $userName = User::model()->findAll();
+// var_dump($userName); die;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<section class="min-vh-100 d-flex align-items-center justify-content-center py-3 text-sm-center p-5 bg-info text-white " style="background: url(images/banner-1920.jpg) no-repeat center; background-size:cover;">
+    <div class="container-fluid">
+        <div class="row justify-content-between align-items-center">
+            <!-- bg-light is background color & p-5 is padding -->
+            <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+            <p class="lead">A One-day Conference About All Things JavaScript!</p>
+        </div>
+    </div>
+</section>
+<!-- <div style="margin-top:55px; padding-top:55px;" class="container-fluid text-sm-center p-5 bg-info text-white ">
+    <!-- bg-light is background color & p-5 is padding -->
+<!-- <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+    <p class="lead">A One-day Conference About All Things JavaScript!</p>
+</div> -->
