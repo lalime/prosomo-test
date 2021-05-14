180b6b11a5ae6fbc8f8c8751cc11a77f4eae5f9d
Lionnel Leunkeu <lleunkeu@gmail.com>
2021-05-14 11:55:23 +0100

    Added database configs

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,7 +2,7 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	/*
 	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
@@ -11,4 +11,9 @@ return array(
 	'password' => '',
 	'charset' => 'utf8',
 	*/
+	'connectionString' => 'mysql:host=localhost;dbname=prosomotest_db',
+	'emulatePrepare' => true,
+	'username' => 'sammy',
+	'password' => 'P@ssw0rd1',
+	'charset' => 'utf8',
 );
