8443ea805ac11952d90f5d76d59dae44b9541a49
Lionnel Leunkeu <lleunkeu@gmail.com>
2021-05-17 08:11:15 +0100

    Generated CRUD files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    console.log( "document loaded" );
+
+    // $('.contact-list tr').click(function() {
+    //     var currentRow = $(this);
+        
+    //     // alert(currentRow.data('comment'));
+    //     $('#myModal .modal-body').html(currentRow.data('comment'));
+    // });
+});
