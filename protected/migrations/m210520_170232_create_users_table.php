9d12faaff674e1a61284e8950667c55d3d49142d
Lionnel Leunkeu <lleunkeu@gmail.com>
2021-05-20 18:04:38 +0100

    Users migration

---
+++ protected/migrations/m210520_170232_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+class m210520_170232_create_users_table extends CDbMigration
+{
+	public function up()
+	{
+        $this->createTable('users', array(
+                'id' => 'pk',
+                'username' => 'string NOT NULL',
+                'password' => 'string NOT NULL',
+                'created_at' => 'datetime DEFAULT CURRENT_TIMESTAMP',
+            )
+        );
+	}
+
+	public function down()
+	{
+        $this->dropTable('contacts');
+		echo "m210520_170232_create_users_table does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
